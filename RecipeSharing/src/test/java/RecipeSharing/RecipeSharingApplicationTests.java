18bed884de74d82fc38679c042bdf29111dbac2d
Lesleyjane <lesleyjane.j@gmail.com>
2022-03-25 20:17:11 +0000

    initial commit. Created a spring project using gradle and java, added spring MVC framework as dependency

---
+++ RecipeSharing/src/test/java/RecipeSharing/RecipeSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package RecipeSharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
