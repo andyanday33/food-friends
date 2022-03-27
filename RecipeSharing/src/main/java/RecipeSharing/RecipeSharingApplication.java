dcb5d6de354e920fb59d2c58207a07107cd7e1ba
Lesleyjane <lesleyjane.j@gmail.com>
2022-03-27 19:14:26 +0100

    Added some empty API methods for:  creating a recipe, retrieving a recipe, getting all recipes for a user, getting all recipes for each cuisine, getting all available cuisines, changing permissions on a user's recipe, getting recipes by specific ingredient.
     added cors mapping.

--- RecipeSharing/src/main/java/RecipeSharing/RecipeSharingApplication.java
+++ RecipeSharing/src/main/java/RecipeSharing/RecipeSharingApplication.java
@@ -2,9 +2,24 @@ package RecipeSharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RecipeSharingApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(final CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*");
+						//.allowedMethods("POST", "GET");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeSharingApplication.class, args);
