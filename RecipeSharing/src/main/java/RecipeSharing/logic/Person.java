ea1fc7894258f6c5bd2b062842defc69050810a5
eh250 <eh250@st-andrews.ac.uk>
2022-03-28 17:33:27 +0100

    Added initial logic to Person class.

--- RecipeSharing/src/main/java/RecipeSharing/logic/Person.java
+++ RecipeSharing/src/main/java/RecipeSharing/logic/Person.java
@@ -1,4 +1,62 @@
 package RecipeSharing.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class for each person, parent class of admin and user
+ */
 public class Person {
+
+    //TODO add something to do with permissions.
+    private String name;
+    private String email;
+    private List<Recipe> recipes = new ArrayList<>();
+    private List<Recipe> readAccess = new ArrayList<>();
+    private List<Recipe> writeAccess = new ArrayList<>();
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public List<Recipe> getReadAccess() {
+        return readAccess;
+    }
+
+    public void setReadAccess(List<Recipe> readAccess) {
+        this.readAccess = readAccess;
+    }
+
+    public List<Recipe> getWriteAccess() {
+        return writeAccess;
+    }
+
+    public void setWriteAccess(List<Recipe> writeAccess) {
+        this.writeAccess = writeAccess;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
