800adaec231783267d79350337019f5c1f6c34e3
eh250 <eh250@st-andrews.ac.uk>
2022-03-28 16:18:50 +0100

    Added Meal class and added further logic to Recipe class.

--- RecipeSharing/src/main/java/RecipeSharing/logic/Recipe.java
+++ RecipeSharing/src/main/java/RecipeSharing/logic/Recipe.java
@@ -2,6 +2,9 @@ package RecipeSharing.logic;
 
 import java.util.List;
 
+/**
+ * Class for recipes, contains all pertinent information.
+ */
 public class Recipe {
 
     private String title;
@@ -11,9 +14,15 @@ public class Recipe {
     private List<Person> readAccess;
     private List<Person> writeAccess;
     private List<Cuisine> cuisines;
+    private Meal meal;
     private List<Ingredient> ingredients;
     private double rating;
     private int numRatings;
+
+    //TODO decide what fields are needed for constructor
+    public Recipe() {
+
+    }
 
     public void updateRating(int userRating) {
         double total = (rating * numRatings) + userRating;
