b6700fe832903786a49524c0c5a167cc62bf9b67
eh250 <eh250@st-andrews.ac.uk>
2022-04-03 09:12:17 +0100

    Added Unit test class.

---
+++ RecipeSharing/src/test/java/recipesharing/logic/UnitTest.java
@@ -0,0 +1,33 @@
+package recipesharing.logic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getSymbol() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void isVolume() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void getConversionQuotient() {
+    }
+}
