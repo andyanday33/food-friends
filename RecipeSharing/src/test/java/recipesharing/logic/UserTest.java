c1c88c5b5fa080e2ba2649de791910f0be0cad5e
eh250 <eh250@st-andrews.ac.uk>
2022-04-03 09:30:31 +0100

    Added a test class for User and abstract superclass Person.

---
+++ RecipeSharing/src/test/java/recipesharing/logic/UserTest.java
@@ -0,0 +1,101 @@
+package recipesharing.logic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addRecipe() {
+    }
+
+    @Test
+    void removeRecipe() {
+    }
+
+    @Test
+    void getAuthoredRecipes() {
+    }
+
+    @Test
+    void setAuthoredRecipes() {
+    }
+
+    @Test
+    void getReadAccess() {
+    }
+
+    @Test
+    void addReadAccess() {
+    }
+
+    @Test
+    void removeReadAccess() {
+    }
+
+    @Test
+    void setReadAccess() {
+    }
+
+    @Test
+    void addWriteAccess() {
+    }
+
+    @Test
+    void removeWriteAccess() {
+    }
+
+    @Test
+    void getWriteAccess() {
+    }
+
+    @Test
+    void setWriteAccess() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void addLikedRecipe() {
+    }
+
+    @Test
+    void removeLikedRecipe() {
+    }
+
+    @Test
+    void getLikedRecipes() {
+    }
+
+    @Test
+    void setLikedRecipes() {
+    }
+
+    @Test
+    void addToHistory() {
+    }
+
+    @Test
+    void getHistory() {
+    }
+}
