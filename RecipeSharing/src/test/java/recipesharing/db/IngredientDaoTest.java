698ca63b901c595ce4481a7957a66ea776e1ece8
eh250 <eh250@st-andrews.ac.uk>
2022-04-04 11:30:34 +0100

    Created initial IngredientDaoTest class.

---
+++ RecipeSharing/src/test/java/recipesharing/db/IngredientDaoTest.java
@@ -0,0 +1,21 @@
+package recipesharing.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import recipesharing.logic.Recipe;
+import recipesharing.logic.RecipeItem;
+import recipesharing.logic.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Testing of Ingredient database access object and its methods.
+ */
+public class IngredientDaoTest {
+
+
+}
