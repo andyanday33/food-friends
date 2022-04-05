70543e922758df65b0e4c69229a94466d3e6d7a7
eh250 <eh250@st-andrews.ac.uk>
2022-04-05 09:58:12 +0100

    Added two additional tests to RecipeDaoTest.

--- RecipeSharing/src/test/java/recipesharing/db/RecipeDaoTest.java
+++ RecipeSharing/src/test/java/recipesharing/db/RecipeDaoTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
+ * Testing of recipe data access object and its methods.
  */
 @SpringBootTest
 class RecipeDaoTest {
@@ -24,16 +24,28 @@ class RecipeDaoTest {
     @Autowired
     UserDao userDao;
 
+    /**
+     * Creates and adds a recipe to the database and checks that it can retrieved and has the correct details.
+     */
     @Test
     void addRecipe() {
         User user = userDao.findUserById("6249cadaa1f0c07dba837007");
-        Recipe recipe = new Recipe("pancake", "authorId1", user, true, true, 0);
+        Recipe recipe = new Recipe("jaffa cake", "authorId1", user, true, true, 0);
         recipeDao.addRecipe(recipe);
+        List<Recipe> recipes = recipeDao.findRecipeByRecipeName("jaffa cake");
+        Recipe retrievedRecipe = recipes.get(0);
+        assertEquals(retrievedRecipe.getRecipeName(), "jaffa cake");
+        assertEquals(retrievedRecipe.getAuthorId(), "authorId1");
     }
 
+    /**
+     * Deletes the recipe created in the previous test and checks that this has been completed.
+     */
     @Test
     void testDeleteRecipeById() {
-
+        recipeDao.deleteRecipeById("624c031ea9531c27eb1c0138");
+        Recipe recipe = recipeDao.findRecipeById("624c031ea9531c27eb1c0138");
+        assertNull(recipe);
     }
 
     @Test
