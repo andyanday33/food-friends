864d947670340150b9c213c8463465ab83b2e4b7
eh250 <eh250@st-andrews.ac.uk>
2022-04-05 20:44:44 +0100

    Added a test for getAllAdmins in AdminControllerTest.

--- RecipeSharing/src/test/java/recipesharing/api/AdminControllerTest.java
+++ RecipeSharing/src/test/java/recipesharing/api/AdminControllerTest.java
@@ -1,15 +1,40 @@
 package recipesharing.api;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import recipesharing.api.RecipeController;
+import static org.junit.Assert.assertEquals;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AdminControllerTest {
+
+    @Autowired
     WebTestClient client;
 
-    @BeforeEach
-    void setup() {
-        client = WebTestClient.bindToController(new RecipeController()).build();
+    @Test
+    public void testLogin() {
+
     }
 
+    @Test
+    public void testLogout() {
 
+    }
+
+    /**
+     * Gets all admins from the API and check it was successful.
+     */
+    //TODO add or test some layer of authentication here to prevent a random user getting all admin details.
+    @Test
+    public void testGetAllAdmins() {
+        client.get().uri("/admin/getAllAdmins").accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("success", "true");
+    }
 }
