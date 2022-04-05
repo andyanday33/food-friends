a00ec03438b65a7f1ac91c0b371110c23fde331c
eh250 <eh250@st-andrews.ac.uk>
2022-04-05 21:03:29 +0100

    Added a logout test method for Admin API.

--- RecipeSharing/src/test/java/recipesharing/api/AdminControllerTest.java
+++ RecipeSharing/src/test/java/recipesharing/api/AdminControllerTest.java
@@ -1,34 +1,42 @@
 package recipesharing.api;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import recipesharing.api.RecipeController;
-import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests the admin related API methods.
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AdminControllerTest {
 
     @Autowired
     WebTestClient client;
 
+    /**
+     * Tests that an admin can log in.
+     */
     @Test
     public void testLogin() {
 
     }
 
+    /**
+     * Tests that the logout method in the API is called successfully.
+     */
     @Test
     public void testLogout() {
-
+        client.post().uri("/admin/logout").accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is2xxSuccessful();
     }
 
     /**
      * Gets all admins from the API and check it was successful.
      */
-    //TODO add or test some layer of authentication here to prevent a random user getting all admin details.
+    //TODO potentially add some layer of authentication here to prevent a random user getting all admin details.
     @Test
     public void testGetAllAdmins() {
         client.get().uri("/admin/getAllAdmins").accept(MediaType.APPLICATION_JSON)
