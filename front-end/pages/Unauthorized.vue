aa58fea8cac78b3c587aac53e8d2c4b7abb99f20
berke <bab26@st-andrews.ac.uk>
2022-04-05 12:48:11 +0300

    Styling changes for unauthorized page

--- front-end/pages/Unauthorized.vue
+++ front-end/pages/Unauthorized.vue
@@ -6,7 +6,7 @@
 <div class="flex-1 p-4 grow shadow justify-start">
         <h2 class="text-5xl text-center">Unauthorized</h2>
         <p class="p-4">
-            Please <a class="color-green-900 hover:text-gray-50 hover:bg-green-900" @click="$auth.loginWith('auth0')">Login</a> or Signup to see this page
+            Please <a href="#" class="lg:inline-block lg:mt-0 hover:text-white py-2 rounded hover:bg-green-900 " @click="$auth.loginWith('auth0')">Login or Signup</a>  to see this page
         </p>
 </div>
 
