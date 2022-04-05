492d109f988d57c4a65bb2fa25227a4a48c88a09
eh250 <eh250@st-andrews.ac.uk>
2022-04-05 21:38:04 +0100

    Added test stubs that check admin log in.

--- RecipeSharing/src/test/java/recipesharing/api/AdminControllerTest.java
+++ RecipeSharing/src/test/java/recipesharing/api/AdminControllerTest.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 /**
  * Tests the admin related API methods.
@@ -16,11 +19,41 @@ public class AdminControllerTest {
     WebTestClient client;
 
     /**
-     * Tests that an admin can log in.
+     * Tests that an existing admin can log in.
      */
     @Test
-    public void testLogin() {
+    //TODO discuss loginService response and then implement.
+    public void testSuccessfulLogin() {
+        URI uri = UriComponentsBuilder.fromPath("/admin/login")
+                .queryParam("email", "admin@st-andrews.ac.uk")
+                .queryParam("password", "password")
+                .build()
+                .toUri();
 
+        /*
+        client.post().uri(uri).accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus();
+         */
+    }
+
+    /**
+     * Tests that a non-admin cannot log in using the admin service.
+     */
+    //TODO discuss loginService response and then implement.
+    @Test
+    public void testUnsuccessfulLogin() {
+        URI uri = UriComponentsBuilder.fromPath("/admin/login")
+                .queryParam("email", "nonsense")
+                .queryParam("password", "cheese")
+                .build()
+                .toUri();
+
+        /*
+        client.post().uri(uri).accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus();
+         */
     }
 
     /**
