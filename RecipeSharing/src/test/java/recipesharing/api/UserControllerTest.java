c126dbb339b5e4cda9d8c930a8551e71e60c792b
eh250 <eh250@st-andrews.ac.uk>
2022-04-06 10:52:26 +0100

    Added inital imports and annotations needed for UserControllerTest.

--- RecipeSharing/src/test/java/recipesharing/api/UserControllerTest.java
+++ RecipeSharing/src/test/java/recipesharing/api/UserControllerTest.java
@@ -1,15 +1,23 @@
 package recipesharing.api;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+
+/**
+ * Tests the admin related API methods.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
+
+    @Autowired
     WebTestClient client;
 
-    @BeforeEach
-    void setup() {
-        client = WebTestClient.bindToController(new RecipeController()).build();
-    }
 
 
 }
