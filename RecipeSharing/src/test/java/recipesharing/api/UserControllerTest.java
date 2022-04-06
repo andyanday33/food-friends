5ccf013faac4378776257182b64afac0f19a31fe
lj77 <lesleyjane.j@gmail.com>
2022-04-06 13:29:44 +0100

    Delete /getRecipe{user} as we have /getRecipesByAuthorId

--- RecipeSharing/src/test/java/recipesharing/api/UserControllerTest.java
+++ RecipeSharing/src/test/java/recipesharing/api/UserControllerTest.java
@@ -25,7 +25,7 @@ public class UserControllerTest {
                 .expectBody()
                 .jsonPath("success", "true");
     }
-
+    */
     /**
      * Tests that users can log out.
      */
@@ -37,7 +37,7 @@ public class UserControllerTest {
                 .expectBody()
                 .jsonPath("success", "false");
     }
-    */
+
 
     /**
      * Tests that users can log out.
