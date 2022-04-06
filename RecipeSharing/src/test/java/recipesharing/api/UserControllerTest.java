1224708f2159945d9b99149102201949700e2132
eh250 <eh250@st-andrews.ac.uk>
2022-04-06 11:49:31 +0100

    Added log in tests to UserControllerTest.

--- RecipeSharing/src/test/java/recipesharing/api/UserControllerTest.java
+++ RecipeSharing/src/test/java/recipesharing/api/UserControllerTest.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 /**
  * Tests the admin related API methods.
@@ -22,15 +19,24 @@ public class UserControllerTest {
      * Tests that users can log in.
      */
     @Test
-    void userLogin() {
-        URI uri = UriComponentsBuilder.fromPath("/user/login")
-                .queryParam("email", "test2@st-andrews.ac.uk")
-                .queryParam("password", "test password")
-                .build()
-                .toUri();
+    void userSuccessfulLogin() {
+        client.post().uri("user/login?email=test2@st-andrews.ac.uk&password=test password").accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("success", "true");
+    }
 
-        client.post().uri(uri).accept(MediaType.APPLICATION_JSON)
-                .exchange();
+    /**
+     * Tests that users can log out.
+     */
+    @Test
+    void userUnsuccessfulLogin() {
+        client.post().uri("user/login?email=garbage&password=garbage").accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("success", "false");
     }
 
     /**
